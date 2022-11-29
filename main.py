fcbd95d3eb3effa482352a806ed31c39218670a9
Jacob <jlh2hb@virginia.edu>
2022-11-29 14:32:57 -0500

    Plot drawing with test data

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import argparse
+import plotly.graph_objects as go
+import plotly.io as pio
 from time import sleep
 import requests
 from collections import defaultdict
+from tqdm import tqdm
 import dns.resolver
 import json
 
@@ -52,34 +55,96 @@ def get_geolocation(ip: str):
     print("Requests being throttled.")
     return None, None
 
+def get_my_ip():
+  url = "https://checkip.amazonaws.com/"
+  response = requests.get(url)
+  assert response.status_code == 200
+  return response.text.strip()
+
 def map_ips_to_geolocation(hosts):
   res = defaultdict(lambda: [])
 
-  for domain, ips in hosts.items():
+  for domain, ips in tqdm(hosts.items()):
     for ip in ips:
       lat, long = get_geolocation(ip)
-      print(f"found {lat}, {long} for ip {ip}")
+      print(f"found {lat}, {long} for domain {domain}")
       res[domain].append([lat, long])
-      sleep(1)
+      sleep(1.3)
 
   print(res)
   return res
 
-def draw_map(geolocations):
+def draw_map(geolocations: dict):
+  my_lat, my_long = get_geolocation(get_my_ip())
+
+  fig = go.Figure()
+
+  for domain, latlong_array in geolocations.items():
+    for lat, long in latlong_array:
+      # Arc
+      fig.add_trace(
+        go.Scattergeo(
+          locationmode = 'USA-states',
+          lon = [my_long, long],
+          lat = [my_lat, lat],
+          mode = 'lines',
+          line = dict(width = 1,color = 'red'),
+          opacity = 1,
+        )
+      )
+      
+      # Endpoint
+      fig.add_trace(go.Scattergeo(
+        locationmode = 'USA-states',
+        lon = [long],
+        lat = [lat],
+        hoverinfo = 'text',
+        text = f"Domain {domain}",
+        mode = 'markers',
+        marker = dict(
+            size = 2,
+            color = 'rgb(255, 0, 0)',
+            line = dict(
+                width = 3,
+                color = 'rgba(68, 68, 68, 0)'
+            )
+        )
+      ))
+
+
+  fig.update_layout(
+    title_text = 'Request Geolocations',
+    showlegend = False,
+    geo = dict(
+        scope = 'north america',
+        projection_type = 'azimuthal equal area',
+        showland = True,
+        landcolor = 'rgb(243, 243, 243)',
+        countrycolor = 'rgb(204, 204, 204)',
+    ),
+)
+
+
+  pio.write_image(fig, './images/test.png', format='png', scale=6, width=1080, height=1080)
+
   pass
 
 def main():
   args = handle_cli_args()
-
   harfile = parse_har_file(args.filename)
-
   hostnames = get_hosts_from_harfile(harfile)
-
   hosts_with_addrs = do_dns_query(hostnames)
+  # geolocations = map_ips_to_geolocation(hosts_with_addrs)
 
-  geolocations = map_ips_to_geolocation(hosts_with_addrs)
+  test_mapping = {
+    "amplify.outbrain.com": [[37.502129, 15.08719]],
+    "w.usabilla.com" : [[39.04372, -77.487488]],
+    "ad.doubleclick.net": [[37.405991, -122.078514]],
+    "cdn.cookielaw.org": [[37.7757, -122.395203]],
+    "bea4.v.fwmrm.net": [[47.627499, -122.346199], [37, -77]],
+  }
 
-  draw_map(geolocations)
+  draw_map(test_mapping)
 
   exit(0)
 
